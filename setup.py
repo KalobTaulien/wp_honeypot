18ddab7fcf93144b0dda2c8639d301ce5fcdd0e3
Kalob Taulien <kalob.taulien@gmail.com>
2018-12-10 16:57:58 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='wp_honeypot',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     include_package_data=True,
     license='MIT',
