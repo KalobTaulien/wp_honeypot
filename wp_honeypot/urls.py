734ad7443eea64a4b1adbc612b78e107c0d9026c
Kalob Taulien <kalob.taulien@gmail.com>
2019-01-22 11:29:44 -0700

    Added common bot urls

--- wp_honeypot/urls.py
+++ wp_honeypot/urls.py
@@ -2,9 +2,20 @@ from django.conf.urls import url
 
 from django.views.generic import RedirectView
 
-from wp_honeypot.models import WPHoneypotLogin
+from wp_honeypot.models import WPHoneypotLogin, WpHoneypotRedirect
 
 urlpatterns = [
-    url(r'^wp-admin/?$', RedirectView.as_view(url='/wp-login.php?reauth=1'), name='wp_honeypot_admin'),
-    url(r"^wp-login.php/?$", WPHoneypotLogin.as_view(), name='wp_honeypot_login'),
+    url(
+        r"^wp-admin/?$",
+        RedirectView.as_view(url="/wp-login.php?reauth=1"),
+        name="wp_honeypot_admin",
+    ),
+    url(r"^wp-login.php/?$", WPHoneypotLogin.as_view(), name="wp_honeypot_login"),
+    url(r"^wp/wp-admin/install.php$", WpHoneypotRedirect.as_view()),
+    url(r"^wp-admin/install.php$", WpHoneypotRedirect.as_view()),
+    url(r"^old/wp-admin/install.php$", WpHoneypotRedirect.as_view()),
+    url(r"^blog/wp-admin/install.php$", WpHoneypotRedirect.as_view()),
+    url(r"^new/wp-admin/install.php$", WpHoneypotRedirect.as_view()),
+    url(r"^test/wp-admin/install.php$", WpHoneypotRedirect.as_view()),
+    url(r"^wordpress/wp-admin/install.php$", WpHoneypotRedirect.as_view()),
 ]
